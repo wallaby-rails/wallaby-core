93246b7f27dcaf86cd74da3d440b9acf4cb8c5cd
Tian Chen <me@tian.im>
2020-04-03 18:29:04 +1100

    chore: specs update

--- spec/services/wallaby/map/mode_mapper_spec.rb
+++ spec/services/wallaby/map/mode_mapper_spec.rb
@@ -2,35 +2,24 @@ require 'rails_helper'
 
 describe Wallaby::Map::ModeMapper do
   describe '.execute' do
-    context 'when given class_names is blank' do
+    context 'when class_names are blank' do
       it 'returns empty hash' do
+        expect(described_class.execute(nil)).to be_a Wallaby::ClassHash
         expect(described_class.execute(nil)).to eq({})
+
+        expect(described_class.execute([])).to be_a Wallaby::ClassHash
         expect(described_class.execute([])).to eq({})
       end
     end
 
-    context 'when mode classes are not empty' do
-      before do
-        Wallaby.configuration.custom_models = [Array, Hash]
-      end
+    context 'when class_names are not blank' do
+      before { Wallaby.configuration.custom_models = [Array, Hash] }
 
       it 'returns a mode map' do
-        expect(described_class.execute(Wallaby::ClassArray.new([Wallaby::Custom]))).to be_a Wallaby::ClassHash
-        expect(described_class.execute(Wallaby::ClassArray.new([Wallaby::Custom]))).to eq \
-          'Array' => 'Wallaby::Custom',
-          'Hash' => 'Wallaby::Custom'
-        expect(described_class.execute(Wallaby::ClassArray.new(['Wallaby::Custom']))).to eq \
-          'Array' => 'Wallaby::Custom',
-          'Hash' => 'Wallaby::Custom'
-      end
-
-      context 'when Array is given' do
-        it 'returns a mode map' do
-          expect(described_class.execute(Wallaby::ClassArray.new([Wallaby::Custom]))).to be_a Wallaby::ClassHash
-          expect(described_class.execute([Wallaby::Custom])).to eq \
-            'Array' => 'Wallaby::Custom',
-            'Hash' => 'Wallaby::Custom'
-        end
+        expect(described_class.execute([Wallaby::Custom])).to be_a Wallaby::ClassHash
+        expect(described_class.execute([Wallaby::Custom])).to eq \
+          Array => Wallaby::Custom,
+          Hash => Wallaby::Custom
       end
     end
   end
