b44e51d493d1954580f9d44717e24aeaf23d8e53
Tian Chen <me@tian.im>
2020-05-03 23:26:55 +1000

    chore: rename max_length to max_text_length

--- lib/wallaby/configuration/metadata.rb
+++ lib/wallaby/configuration/metadata.rb
@@ -9,10 +9,10 @@ module Wallaby
       # @!attribute [w] max
       def max=(_max)
         Deprecator.alert 'config.metadata.max=', from: '0.3.0', alternative: <<~INSTRUCTION
-          Please set .max_length= from the controller instead, for example:
+          Please set .max_text_length= from the controller instead, for example:
 
             class Admin::ApplicationController < Wallaby::ResourcesController
-              self.max_length = 50
+              self.max_text_length = 50
             end
         INSTRUCTION
       end
@@ -21,7 +21,7 @@ module Wallaby
       # @!attribute [r] max
       def max
         Deprecator.alert 'config.metadata.max', from: '0.3.0', alternative: <<~INSTRUCTION
-          Please use controller_class.max_length instead.
+          Please use controller_class.max_text_length instead.
         INSTRUCTION
       end
     end
