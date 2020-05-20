fe4473f2003fa2d29619ff16ec28e7bb2f57afee
Tian Chen <me@tian.im>
2020-05-20 21:03:02 +1000

    chore: refactor generators and move common codes into application generator

--- lib/generators/wallaby/engine/servicer/servicer_generator.rb
+++ lib/generators/wallaby/engine/servicer/servicer_generator.rb
@@ -1,32 +1,18 @@
 # frozen_string_literal: true
 
+require_relative '../application_generator'
+
 module Wallaby
   class Engine
     # `wallaby:engine:servicer` generator
-    class ServicerGenerator < Rails::Generators::NamedBase
+    # @see https://github.com/wallaby-rails/wallaby-core/blob/master/lib/generators/wallaby/engine/servicer/USAGE
+    class ServicerGenerator < ApplicationGenerator
       source_root File.expand_path('templates', __dir__)
-      argument :name, type: :string
-      argument :parent_name, type: :string, default: nil, required: false
-
-      # @see https://github.com/wallaby-rails/wallaby-core/blob/master/lib/generators/wallaby/engine/servicer/USAGE
-      def install
-        template 'servicer.rb.erb', "app/servicers/#{name}_servicer.rb"
-      end
 
       protected
 
-      def servicer_class
-        "#{class_name}Servicer"
-      end
-
-      def parent_servicer_class
-        return "#{parent_name.classify}Servicer" if parent_name
-
-        Wallaby.configuration.resources_controller.application_servicer
-      end
-
-      def model_name
-        class_name.singularize
+      def base_name
+        'servicer'
       end
     end
   end
