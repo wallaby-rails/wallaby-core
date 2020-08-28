4e7ddf3a00eb96b31f8b78eabadbde5c42cf04bc
Tian Chen <me@tian.im>
2020-08-29 00:34:32 +1000

    chore: improve engine name finder (#48)
    * chore: improve engine name finder
    
    * chore: update document
    
    * chore: update docs again

--- lib/services/wallaby/engine_name_finder.rb
+++ lib/services/wallaby/engine_name_finder.rb
@@ -4,17 +4,17 @@ module Wallaby
   # Service object to find the engine name by given request environment variables.
   class EngineNameFinder
     class << self
-      # Use script name to find out the corresponding named route and its engine name.
+      # Loop through all the routes and find out the engine who matches the given request path.
       #
-      # When it can't find the engine name, it will return empty string. Reason is to prevent it from being run again.
-      # @param env [Hash, String] request env or script name
-      # @return [String] engine name or empty string if not found
-      def find(env)
-        script_name = env[SCRIPT_NAME] || env[PATH_INFO] if env.is_a? Hash
-        script_name = env if env.is_a? String
-        return EMPTY_STRING if script_name.blank?
-
-        named_route = Rails.application.routes.routes.find { |route| route.path.match(script_name) }
+      # When it can't find the engine name, it will return empty string
+      # to prevent it from being run again.
+      # @param request_path [String] request path
+      # @return [String] engine name if found
+      # @return [String] empty string if not found
+      def find(request_path)
+        named_route = Rails.application.routes.routes.find do |route|
+          route.path.match(request_path) && route.app.app == Wallaby::Engine
+        end
         named_route.try(:name) || EMPTY_STRING
       end
     end
