fe34e82894d9d1f4775fc06820646d5dae36f2d1
Tian Chen <me@tian.im>
2020-08-28 22:31:10 +1000

    chore: improve engine name finder

--- lib/concerns/wallaby/engineable.rb
+++ lib/concerns/wallaby/engineable.rb
@@ -35,7 +35,7 @@ module Wallaby
     # If `/inner` is current script name, then `current_engine_name` returns `'inner_engine'`.
     # @return [String] engine name for current request
     def current_engine_name
-      @current_engine_name ||= controller_configuration.engine_name || EngineNameFinder.find(request.env)
+      @current_engine_name ||= controller_configuration.engine_name || EngineNameFinder.find(request.path)
     end
   end
 end
