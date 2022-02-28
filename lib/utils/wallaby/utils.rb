a585feb92c16ced20d7ee79b88a1d2c64249403b
Tian Chen <me@tian.im>
2022-02-28 22:55:20 +1100

    fix: proper clone solution

--- lib/utils/wallaby/utils.rb
+++ lib/utils/wallaby/utils.rb
@@ -1,22 +1,34 @@
 # frozen_string_literal: true
 
 module Wallaby
-  # Utils
-  module Utils
-    # @see http://stackoverflow.com/a/8710663/1326499
+  module Utils # :nodoc:
     # @param object [Object]
-    # @return [Object] a clone object
+    # @return [Object] a cloned object
     def self.clone(object)
-      object.deep_dup
+      # NOTE: Neither marshal/deep_dup/dup is able to make a correct deep copy,
+      # therefore, this is our solution:
+      case object
+      when Hash
+        # NOTE: `default` should be cloned as well
+        object.each_with_object(object.class.new(object.default)) { |(key, value), hash| hash[key] = clone(value) }
+      when Array
+        object.each_with_object(object.class.new) { |value, array| array << clone(value) }
+      when Class
+        # NOTE: `Class.dup` turns the origin Class object into an anonymous clone.
+        # therefore, the Class object itself should be returned instead
+        object
+      else
+        object.dup
+      end
     end
 
     # @param object [Object, nil]
     # @return [String] inspection string for the given object
     def self.inspect(object)
-      return 'nil' unless object
-      return object.name if object.is_a? Class
+      return 'nil' if object.nil?
+      return "#{object.class}##{object.try(:id)}" if object.is_a?(::ActiveRecord::Base)
 
-      "#{object.class}##{object.try(:id)}"
+      object.inspect
     end
   end
 end
--- spec/utils/wallaby/utils_spec.rb
+++ spec/utils/wallaby/utils_spec.rb
@@ -1,9 +1,45 @@
 require 'rails_helper'
 
 describe Wallaby::Utils do
+  describe '.clone' do
+    it 'clones object' do
+      object = 'Post'
+      expect(described_class.clone(object)).to eq object
+      expect(described_class.clone(object).object_id).not_to eq object.object_id
+
+      object = { a: 1 }
+      expect(described_class.clone(object)).to eq object
+      expect(described_class.clone(object)[:a]).to eq object[:a]
+      expect(described_class.clone(object).object_id).not_to eq object.object_id
+
+      object.default = 2
+      expect(described_class.clone(object)).to eq object
+      expect(described_class.clone(object)[:a]).to eq object[:a]
+      expect(described_class.clone(object).default).to eq object.default
+      expect(described_class.clone(object).object_id).not_to eq object.object_id
+
+      object = ['a']
+      expect(described_class.clone(object)).to eq object
+      expect(described_class.clone(object)[0]).to eq object[0]
+      expect(described_class.clone(object)[0].object_id).not_to eq object[0].object_id
+      expect(described_class.clone(object).object_id).not_to eq object.object_id
+
+      object = String
+      expect(described_class.clone(object)).to eq object
+      expect(described_class.clone(object).object_id).to eq object.object_id
+
+      object = AllPostgresType.create text: 'test'
+      expect(described_class.clone(object)).not_to eq object
+      expect(described_class.clone(object).object_id).not_to eq object.object_id
+      expect(described_class.clone(object).id).to be_nil
+      expect(described_class.clone(object).attributes.except('id')).to eq object.attributes.except('id')
+    end
+  end
+
   describe '.inspect' do
     it 'returns filter name' do
       expect(described_class.inspect(nil)).to eq 'nil'
+      expect(described_class.inspect(false)).to eq 'false'
       expect(described_class.inspect(Person.new(id: 1))).to eq 'Person#1'
     end
   end
