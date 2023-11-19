6ca4205381622549e4e3a6e090ae19c5fb3acfe5
Tian Chen <me@tian.im>
2023-11-19 17:17:01 +1100

    Fix for Rails 7.1.2 moving app folders out of eager_load_paths

--- lib/wallaby/preloader.rb
+++ lib/wallaby/preloader.rb
@@ -39,7 +39,10 @@ module Wallaby
     # @!attribute [r] eager_load_paths
     # @return [Array<String, Pathname>]
     def eager_load_paths # :nodoc:
-      @eager_load_paths ||= Rails.configuration.eager_load_paths
+      @eager_load_paths ||=
+        Rails.configuration.paths['app'].expanded
+          .concat(Rails.configuration.eager_load_paths)
+          .uniq
     end
 
     # @!attribute [w] model_paths
